0bb7298857cb8c4279890594c5e18928000d514f
Camilo Sodo <sodocamilo@gmail.com>
2024-06-01 13:52:21 -0300

    Menu de seleccion de cantidad - Cliente

--- Main.cs
+++ Main.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace Tp2AAT {
 
@@ -6,32 +7,33 @@ namespace Tp2AAT {
 
     private const string SI_STR = "SI";
     private const string NO_STR = "NO";
-    private static Tienda tienda = new Tienda();
+    private static Tienda tienda = AgregarProductosDefault(new Tienda());
     private static Carrito carrito = new Carrito();
 
 
-    private static void AgregarProductosDefault(){
-      tienda.agregarProducto(new Producto("Desodorante", 5000, 50));
-      tienda.agregarProducto(new Producto("Vaso", 1000, 13));
-      tienda.agregarProducto(new Producto("Lampara", 30000, 55));
-      tienda.agregarProducto(new Producto("Vino", 10000, 38));
-      tienda.agregarProducto(new Producto("Celular", 1000000, 74));
-      tienda.agregarProducto(new Producto("Monitor", 500000, 67));
-      tienda.agregarProducto(new Producto("Panel de Vidro", 60000, 94));
-      tienda.agregarProducto(new Producto("Cinturon", 8000, 27));
-      tienda.agregarProducto(new Producto("Block de Notas", 3000, 43));
-      tienda.agregarProducto(new Producto("Tablon", 10000, 85));
-      tienda.agregarProducto(new Producto("Pajita", 700, 34));
-      tienda.agregarProducto(new Producto("Coca-Cola", 1500, 52));
-      tienda.agregarProducto(new Producto("Lapicera", 900, 91));
-      tienda.agregarProducto(new Producto("Cinta Adhesiva", 800, 16));
-      tienda.agregarProducto(new Producto("Zapatilla", 7500, 1));
-      tienda.agregarProducto(new Producto("Paraguas", 9200, 7));
-      tienda.agregarProducto(new Producto("Polo", 35000, 12));
-      tienda.agregarProducto(new Producto("Ventilador", 40000, 73));
-      tienda.agregarProducto(new Producto("Almohada", 6800, 29));
-      tienda.agregarProducto(new Producto("Sabana", 7200, 23));
-      tienda.agregarProducto(new Producto("Mochila", 6000, 89));
+    private static Tienda AgregarProductosDefault(Tienda _tienda){
+      _tienda.AgregarProducto(new Producto("Desodorante", 5000, 50));
+      _tienda.AgregarProducto(new Producto("Vaso", 1000, 13));
+      _tienda.AgregarProducto(new Producto("Lampara", 30000, 55));
+      _tienda.AgregarProducto(new Producto("Vino", 10000, 38));
+      _tienda.AgregarProducto(new Producto("Celular", 1000000, 74));
+      _tienda.AgregarProducto(new Producto("Monitor", 500000, 67));
+      _tienda.AgregarProducto(new Producto("Panel de Vidro", 60000, 94));
+      _tienda.AgregarProducto(new Producto("Cinturon", 8000, 27));
+      _tienda.AgregarProducto(new Producto("Block de Notas", 3000, 43));
+      _tienda.AgregarProducto(new Producto("Tablon", 10000, 85));
+      _tienda.AgregarProducto(new Producto("Pajita", 700, 34));
+      _tienda.AgregarProducto(new Producto("Coca-Cola", 1500, 52));
+      _tienda.AgregarProducto(new Producto("Lapicera", 900, 91));
+      _tienda.AgregarProducto(new Producto("Cinta Adhesiva", 800, 16));
+      _tienda.AgregarProducto(new Producto("Zapatilla", 7500, 1));
+      _tienda.AgregarProducto(new Producto("Paraguas", 9200, 7));
+      _tienda.AgregarProducto(new Producto("Polo", 35000, 12));
+      _tienda.AgregarProducto(new Producto("Ventilador", 40000, 73));
+      _tienda.AgregarProducto(new Producto("Almohada", 6800, 29));
+      _tienda.AgregarProducto(new Producto("Sabana", 7200, 23));
+      _tienda.AgregarProducto(new Producto("Mochila", 6000, 89));
+      return _tienda;
     }  
 
     private static bool esVendedor(){
@@ -53,13 +55,12 @@ namespace Tp2AAT {
     }
 
     private static void printMenuVendedor(){
-      string[] lineas = new string[] {"¿Que opcion quieres realizar?\n", " \tAgregar producto\n", " \tEliminar producto\n", " \tVer listado de productos y stock\n", " \tVer dinero en caja\n", " \tSalir\n"};
-      int chars_escritos = 0;
-
-      foreach (string linea in lineas) {
-        chars_escritos += linea.Length;
-        Console.Write(linea);
-      }
+      Console.WriteLine("¿Que opcion quieres realizar?");
+      Console.WriteLine(" \tAgregar producto");
+      Console.WriteLine(" \tEliminar producto");
+      Console.WriteLine(" \tVer listado de productos y stock");
+      Console.WriteLine(" \tVer dinero en caja");
+      Console.WriteLine(" \tSalir");
     }
 
     private static void cambiarSeleccion(int seleccionado, int cant_opciones){
@@ -88,18 +89,18 @@ namespace Tp2AAT {
       string stock = Console.ReadLine();
 
       Producto prod = new Producto(nombre, Double.Parse(costo), Int64.Parse(stock));
-      tienda.agregarProducto(prod);
+      tienda.AgregarProducto(prod);
     }
 
     private static void EliminarProductoMenu(){
       Console.Write("Nombre del producto a eliminar:");
       string nombre = Console.ReadLine();
 
-      tienda.eliminarProducto(nombre);
+      tienda.EliminarProducto(nombre);
     }
 
     private static void printStock(){
-      string stock = tienda.contenidosComoString();
+      string stock = tienda.ContenidosComoString();
       Console.Write(stock);
     }
 
@@ -123,7 +124,7 @@ namespace Tp2AAT {
       }
     }
 
-    private static void menuVendedor(){
+    private static void MenuVendedor(){
       const int cant_opciones = 5;
       int seleccionado = 0;
       printMenuVendedor();
@@ -145,11 +146,14 @@ namespace Tp2AAT {
             printMenuVendedor();
             cambiarSeleccion(seleccionado, cant_opciones);
             break;
+          default:
+            break;
         }
       }
     }
 
     private static void printMenuCliente(List<string> productos){
+      Console.WriteLine("¿Que quiere Agregar al carrito?");
 
       foreach (string producto in productos) {
         Console.WriteLine(" \t" + producto);
@@ -159,37 +163,109 @@ namespace Tp2AAT {
       Console.WriteLine(" \tSalir");
     }
 
-    private static void menuCliente() {
+    private static void CambiarCantidad((int Left, int Top) locacion_cantidad, (int Left, int Top) locacion_precio, int cantidad, double precio){
+      (int Left, int Top) cursor_guardado = (Console.CursorLeft, Console.CursorTop);
+      Console.SetCursorPosition(locacion_cantidad.Left, locacion_cantidad.Top);
+      Console.Write(cantidad + " > ");
+      Console.SetCursorPosition(locacion_precio.Left, locacion_precio.Top);
+      Console.Write(cantidad * precio + "$) ");
+      Console.Write(new string(' ', Console.WindowWidth - Console.CursorLeft)); 
+      Console.SetCursorPosition(cursor_guardado.Left, cursor_guardado.Top);
+    }
+
+    private static void AgregarAlCarritoMenu(string nombre){
+      Producto prod = tienda.ConsultarProducto(nombre);
+      int cantidad_compra = 0;
+      int seleccionado = 0;
+      int cantidad_left_pos, precio_left_pos;
+      Console.WriteLine("");
+      Console.WriteLine($"{nombre}\t\t{prod.precio}$");
+
+      Console.Write(" \t< "); cantidad_left_pos = Console.CursorLeft ; Console.WriteLine(cantidad_compra + " >");
+      Console.Write(" \tAgregar al carrito ("); precio_left_pos = Console.CursorLeft; Console.WriteLine((prod.precio * cantidad_compra) + "$)");
+      Console.WriteLine(" \tCancelar");
+
+      cambiarSeleccion(seleccionado, 3);
+      while(true) {
+        ConsoleKey tecla = esperarTecla();
+        switch (tecla) {
+          case ConsoleKey.UpArrow:
+            seleccionado = seleccionado - 1 >= 0 ? seleccionado - 1 : 0;
+            cambiarSeleccion(seleccionado, 3);
+            break;
+          case ConsoleKey.DownArrow:
+            seleccionado = seleccionado + 1 < 3 ? seleccionado + 1 : 3 - 1;
+            cambiarSeleccion(seleccionado, 3);
+            break;
+          case ConsoleKey.LeftArrow:
+            if (seleccionado == 0) {
+              cantidad_compra = cantidad_compra - 1 >= 0 ? cantidad_compra - 1 : 0;
+              CambiarCantidad((cantidad_left_pos, Console.CursorTop - 3), (precio_left_pos, Console.CursorTop - 2), cantidad_compra, prod.precio);
+            }
+            break;
+          case ConsoleKey.RightArrow:
+            if (seleccionado == 0) {
+              cantidad_compra = cantidad_compra + 1;
+              CambiarCantidad((cantidad_left_pos, Console.CursorTop - 3), (precio_left_pos, Console.CursorTop - 2), cantidad_compra, prod.precio);
+            }
+            break;
+          case ConsoleKey.Enter:
+            break;
+          default:
+            break;
+        }
+      } 
+    }
+
+    private static void MenuCliente() {
       const int max_pagina = 10;
+      List<string> productos = tienda.ConsultarNombres();
+      int cant_opciones = productos.Count + 3;
       int seleccionado = 0;
       int pagina = 0;
 
-      List<string> productos = tienda.ConsultarNombres();
-      
-      Console.WriteLine("¿Que desea comprar?");
+      printMenuCliente(productos);
+      cambiarSeleccion(0, cant_opciones);
 
+      while(true) {
+        ConsoleKey tecla = esperarTecla();
+
+        switch (tecla) {
+          case ConsoleKey.UpArrow:
+            seleccionado = seleccionado - 1 >= 0 ? seleccionado - 1 : 0;
+            cambiarSeleccion(seleccionado, cant_opciones);
+            break;
+          case ConsoleKey.DownArrow:
+            seleccionado = seleccionado + 1 < cant_opciones ? seleccionado + 1 : cant_opciones - 1;
+            cambiarSeleccion(seleccionado, cant_opciones);
+            break;
+          case ConsoleKey.Enter:
+            if (seleccionado < productos.Count) {
+              AgregarAlCarritoMenu(productos[seleccionado]);
+            } else if (seleccionado == cant_opciones - 2) {
+
+            } else if (seleccionado == cant_opciones - 1) {
+
+            } else {
+
+            }
+            break;
+          default:
+            break;
+        }
+
+      }
 
 
     }
 
     public static void Main() {
-      AgregarProductosDefault();
       Console.WriteLine("Bienvenido a la tienda Mauricio Shop");
       string respuesta = "";
       if (esVendedor()) {
-        while(respuesta.ToUpper() != "SALIR") {  
-          menuVendedor();
-            
-          if(respuesta == "1"){
-            Console.WriteLine("Ingrese el nombre del producto");
-            string nombreProd = Console.ReadLine();
-          }
-          if(respuesta == "2");
-          if(respuesta == "3");
-          if(respuesta == "4");
-        }       
+        MenuVendedor();
       } else { 
-            
+        MenuCliente();
       }
     }
   }
